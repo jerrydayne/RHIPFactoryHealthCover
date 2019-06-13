5243e8eea3ade3f2e92dae940195f1d33214a0a3
Jerry <jerrydaexplorer@gmail.com>
2019-06-13 17:56:11 +0100

    updated interface

--- js/need.js
+++ js/need.js
@@ -2,32 +2,45 @@
 function loginFunction(e) {
     e.preventDefault();
 
-  //  $.post('https://healthcover.herokuapp.com/login', {
-      
-    $.post('http://192.168.0.100:7221/login', {
-            phoneNumber: $('#phoneNumber').val(),
-            password: $('#password').val(),
+  //  $.post('https://healthcoverapi.herokuapp.com/login', {
+    $.post('http://localhost:7122/login', {
+            phoneNumber: $('#phoneNumberL').val(),
+            password: $('#passwordL').val(),
         },
         function (response, status) {
+          console.log(status)
             if (status === 'success') {
                 
-        if ($('#userType').val() === 'individual'){
+        if ($('#userTypeL').val() === 'user'){ //login as individual User
           localStorage.setItem('token', response.token);
           localStorage.setItem('id', response.data._id);
           localStorage.setItem('fullName', response.data.fullName);
-          localStorage.setItem('userType', 'individual')
-    console.log(localStorage)
-    console.log($('#userType').val())
+          localStorage.setItem('userType', 'user')
+          console.log($('#userType').val())
           return location.pathname = "dashBoard/userDB.html";
-        } else if  ($('#userType').val() === 'hmo'){
+        } else if  ($('#userTypeL').val() === 'hmo'){ //login as HMO
+          console.log(response)
           localStorage.setItem('token', response.token);
           localStorage.setItem('id', response.data._id);
           localStorage.setItem('fullName', response.data.fullName);
           localStorage.setItem('userType', 'hmo')
           return location.pathname = "HMOdb/hmoDB.html";
+        } else if  ($('#userTypeL').val() === 'hcp'){ //login as HCP
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('id', response.data._id);
+          localStorage.setItem('fullName', response.data.fullName);
+          localStorage.setItem('userType', 'hcp')
+          return location.pathname = "HCPdb/HCPDB.html";
+        } else if  ($('#userTypeL').val() === 'mha'){ //login as MHA
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('id', response.data._id);
+          localStorage.setItem('fullName', response.data.fullName);
+          localStorage.setItem('userType', 'mha')
+          return location.pathname = "MHAdb/MHADB.html";
         }
             }
         })
+        console.log(localStorage);
 
 }
 
@@ -38,35 +51,77 @@ function loginFunction(e) {
         fullName: `${$('#fName').val()} ${$('#lName').val()}`,
         phoneNumber: $('#phoneNumber').val(),
         password: $('#password').val(),
-        
     }
-    if($('#userType').val() === 'individual'){
+
+    //signup as individual user
+    if($('#userType').val() === 'user'){
       return(
-        $.post('http://192.168.0.100:7221/user',
+       // $.post('https://healthcoverapi.herokuapp.com/user',
+        $.post('http://localhost:7122/user',
         sendObj,
         function (response, status) {
             let userData = response.data;
             if (userData) {
               localStorage.setItem('token', response.data);
-                location.pathname = "/login.html";
+              localStorage.setItem('id', response.data._id);
+              localStorage.setItem('userType', 'user')
+              location.pathname = "/regOk.html";
             }
         }))
     }
-    //console.log(sendObj);
 
+    //signup as HMO
     if($('#userType').val() === 'hmo'){
       return(
-      $.post('http://192.168.0.100:7221/hmo',
+      //$.post('https://healthcoverapi.herokuapp.com/hmo',
+      $.post('http://localhost:7122/hmo',
         sendObj,
         function (response, status) {
             let userData = response.data;
             if (userData) {
               localStorage.setItem('token', response.data);
-                location.pathname = "/login.html";
+              localStorage.setItem('id', response.data._id);
+              localStorage.setItem('userType', 'hmo')
+              location.pathname = "/regOk.html";
             }
         }))
     }
     console.log(Storage);
+
+    //signup as HCP
+    if($('#userType').val() === 'hcp'){
+      return(
+      //$.post('https://healthcoverapi.herokuapp.com/hcp',
+      $.post('http://localhost:7122/hcp',
+        sendObj,
+        function (response, status) {
+            let userData = response.data;
+            if (userData) {
+              localStorage.setItem('token', response.data);
+              localStorage.setItem('id', response.data._id);
+              localStorage.setItem('userType', 'hcp')
+              location.pathname = "/regOk.html";
+            }
+        }))
+    }
+
+    //signup as MHA
+    if($('#userType').val() === 'mha'){
+      return(
+     // $.post('https://healthcoverapi.herokuapp.com/mha',
+      $.post('http://localhost:7122/mha',
+        sendObj,
+        function (response, status) {
+            let userData = response.data;
+            if (userData) {
+              localStorage.setItem('token', response.data);
+              localStorage.setItem('id', response.data._id);
+              localStorage.setItem('userType', 'mha')
+              location.pathname = "/regOk.html";
+            }
+        }))
+    }
+
 }
 
 //CreatePlan
@@ -78,59 +133,47 @@ function createPlanFunction(e) {
   feature: $('#feature').val(),
   benefit: $('#benefit').val(),
   exclusion: $('#exclusion').val(),
+  aboutUs: $('#aboutUs').val(),
   priceNaira: $('#priceNaira').val(),
   priceDollar: $('#priceDollar').val(),
   hmo: localStorage.getItem('id')
   }
   console.log(sendObj);
-      $.post('http://192.168.0.100:7221/plan',
+      //$.post('https://healthcoverapi.herokuapp.com/plan',
+      $.post('http://localhost:7122/plan',
       sendObj,
       function (response, status) {
           let userData = response.data;
          console.log('object', userData)
-
-
-          // if (userData) {
-          //   localStorage.setItem('token', response.data);
-          //     location.pathname = "/healthPlans.html";
-          // // };
-         
-          // }
       })
   }
 
-  //getting HMO plan
-  // var planHMO = new XMLHttpRequest();
-  //     planHMO.open ('GET', `http://192.168.0.100:7221/getHMOPlan/5cf0f23c04e35a710dae1ecc`);
-
-  //     planHMO.onreadystaechange = function (){
-  //       var DONE = 4;
-  //       var OK = 200;
-  //       if (planHMO.readystate === DONE){
-       
-  //       //  document.getElementById('summary').innerHTML = 
-  //         if(hmoPlan.status === OK){
-  //           console.log(planHMO.responseText);
-  //         } else {
-  //           console.log('Error: ' + planHMO.status)
-  //         }
-  //       }
-  //     };
-  //   planHMO.send(null);
-
+  //edit User Profile
+function editUserProfileFunction(e) {
+  e.preventDefault();
+  const sendObj = {
+  planType: $('#planType').val(),
+  summary: $('#summary').val(),
+  feature: $('#feature').val(),
+  benefit: $('#benefit').val(),
+  exclusion: $('#exclusion').val(),
+  aboutUs: $('#aboutUs').val(),
+  priceNaira: $('#priceNaira').val(),
+  priceDollar: $('#priceDollar').val(),
+  user: localStorage.getItem('id')
+  }
+  console.log(sendObj);
+      //$.post('https://healthcoverapi.herokuapp.com/plan',
+      $.post('http://localhost:7122/plan',
+      sendObj,
+      function (response, status) {
+          let userData = response.data;
+         console.log('object', userData)
+      })
+  }
 
 
   $(document).ready(function (){
     $('#hmoID').val(localStorage.getItem('id'))
   })
 
-  //display HMO
- 
-// function dashBoardFunction(e) {
-//     e.preventDefault();
-//     if (!localStorage.getItem('token')) {
-//         location.pathname = "dashBoard/userDB.html";
-//     }
-//     console.log(localStorage.getItem('token'))
-//     localStorage.removeItem('token')
-// }
